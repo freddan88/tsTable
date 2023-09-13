4e0cd3529b22501836612aa449b0b15ca657c5e3
freddan88 <fredrik@leemann.se>
2023-09-13 20:49:05 +0200

    Updated types and data i table

--- src/components/Table/helpers/tableData.ts
+++ src/components/Table/helpers/tableData.ts
@@ -3,14 +3,14 @@ import { TableRowType } from "./tableTypes";
 export const tableData: TableRowType[] = [
   {
     id: "6ce2ded8-10dc-4130-b0b6-aa5fa291f5f0",
-    first_name: "Fredrik",
-    last_name: "Leemann",
+    first_name: "Person_1",
+    last_name: "One",
     age: 32,
   },
   {
     id: "f4d15326-1432-4f11-a243-8349b81d908b",
-    first_name: "Elin",
-    last_name: "Leemann",
+    first_name: "Person_2",
+    last_name: "Two",
     age: null,
   },
 ];
--- src/components/Table/helpers/tableTypes.ts
+++ src/components/Table/helpers/tableTypes.ts
@@ -11,8 +11,8 @@ export interface CaptionType {
 
 export interface TableRowType {
   id: string;
-  first_name: "Fredrik" | "Elin";
-  last_name: "Leemann";
+  first_name: "Person_1" | "Person_2";
+  last_name: "One" | "Two";
   age: number | null;
 }
 
