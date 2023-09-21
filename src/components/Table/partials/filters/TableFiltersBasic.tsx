63b0ad95ec1a4e18af528061ff5ab78ffeab67b8
freddan88 <fredrik@leemann.se>
2023-09-21 21:33:08 +0200

    Organized files in folders

---
+++ src/components/Table/partials/filters/TableFiltersBasic.tsx
@@ -0,0 +1,19 @@
+export interface BasicFilterProps {
+  searchPlaceholder?: string;
+}
+
+export default function TableFiltersBasic({
+  searchPlaceholder = "Search...",
+}: BasicFilterProps) {
+  return (
+    <div className="px-6 py-3" data-testId="table-filter-basic">
+      <label htmlFor="table-filter-basic-search">TableFiltersBasic</label>
+      <input
+        id="table-filter-basic-search"
+        placeholder={searchPlaceholder}
+        name="search"
+        type="search"
+      />
+    </div>
+  );
+}
