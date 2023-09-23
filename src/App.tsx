bc8eb4f38aa5e8cd911a512723ae9c5861079f47
freddan88 <fredrik@leemann.se>
2023-09-23 11:24:01 +0200

    Add route and child-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import { useEffect, useState } from "react";
-import Table from "./components/Table/Table";
-import TableColumnsApi from "./components/Table/helpers/TableColumnsApi";
-import { UserResponse } from "./components/Table/helpers/tableTypes";
+import { Route, Routes } from "react-router-dom";
+import UserPage from "./pages/users/UserPage";
 
 function App() {
-  const [data, setData] = useState<UserResponse[]>([]);
-  const [modal, setModal] = useState<UserResponse>();
-
-  useEffect(() => {
-    const controller = new AbortController();
-    fetch("https://jsonplaceholder.typicode.com/users", {
-      signal: controller.signal,
-    })
-      .then((response) => response.json())
-      .then((json: UserResponse[]) => setData(json))
-      .catch((error) => console.error(error));
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!modal) return;
-    console.log(modal);
-  }, [modal]);
-
   return (
     <div className="max-w-4xl mx-auto">
-      <Table
-        table={{
-          columns: TableColumnsApi({ setModal }),
-          data: data,
-        }}
-      />
+      <Routes>
+        <Route path="/" element={<UserPage />}>
+          <Route path=":id" element={<h1>Hello...</h1>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
