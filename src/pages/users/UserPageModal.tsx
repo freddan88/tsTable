03f876e815273bed8cfafd708542409a372c4fda
freddan88 <fredrik@leemann.se>
2023-09-23 18:28:16 +0200

    Work with modals

--- src/pages/users/UserPageModal.tsx
+++ src/pages/users/UserPageModal.tsx
@@ -1,9 +1,29 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useBasePath from "../../shared/hooks/useBasePath";
+import Modal from "../../shared/components/modal/Modal";
+import { useCallback } from "react";
 
 export default function UserPageModal() {
   const params = useParams<{ id: string }>();
   const basePath = useBasePath(params);
+  const navigate = useNavigate();
 
-  return <Link to={basePath}>{params.id}</Link>;
+  const handleClose = useCallback(() => {
+    navigate(basePath, {
+      preventScrollReset: true,
+      replace: true,
+    });
+  }, [basePath, navigate]);
+
+  return (
+    <Modal onClose={handleClose}>
+      <div>{params.id}</div>
+      <footer className="py-2 flex justify-between items-center justify-self-end">
+        <button type="button" onClick={handleClose}>
+          Cancel
+        </button>
+        <button type="submit">Save</button>
+      </footer>
+    </Modal>
+  );
 }
--- src/shared/components/modal/Modal.tsx
+++ src/shared/components/modal/Modal.tsx
@@ -2,8 +2,29 @@ import { ReactNode } from "react";
 
 type ModalType = {
   children: ReactNode;
+  onClose: () => void;
 };
 
-export default function Modal({ children }: ModalType) {
-  return <div>{children}</div>;
+export default function Modal({ children, onClose }: ModalType) {
+  return (
+    <div
+      data-testid="modal-backdrop"
+      className="fixed inset-0 bg-black bg-opacity-80"
+      onClick={onClose}
+    >
+      <section
+        data-testid="modal-main"
+        className="bg-slate-200 rounded-md p-4 max-w-[800px] mx-auto mt-20"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <header className="py-2 flex justify-between items-center">
+          <h2>Test...</h2>
+          <button type="button" onClick={onClose}>
+            &#x2716;
+          </button>
+        </header>
+        {children}
+      </section>
+    </div>
+  );
 }
