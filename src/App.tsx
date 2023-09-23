09899e82aed28351d6578b65d4cd46836713b620
freddan88 <fredrik@leemann.se>
2023-09-23 12:10:11 +0200

    Add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/users/UserPage";
+import TestPage from "./pages/test/TestPage";
+import Modal from "./components/modal/Modal";
 
 function App() {
   return (
     <div className="max-w-4xl mx-auto">
       <Routes>
         <Route path="/" element={<UserPage />}>
-          <Route path=":id" element={<h1>Hello...</h1>} />
+          <Route path=":id" element={<Modal />} />
         </Route>
+        <Route path="/test" element={<TestPage />} />
       </Routes>
     </div>
   );
