dac67e768acdb1943dee6e96269b0df5d05d81fe
freddan88 <fredrik@leemann.se>
2023-09-23 11:25:39 +0200

    Create and rename files to with types

---
+++ src/pages/users/helpers/UserPage.types.ts
@@ -0,0 +1,29 @@
+interface Address {
+  city: string;
+  geo: Geo;
+  street: string;
+  suite: string;
+  zipcode: string;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Company {
+  bs: string;
+  catchPhrase: string;
+  name: string;
+}
+
+export interface UserResponse {
+  address: Address;
+  company: Company;
+  email: string;
+  id: number;
+  name: string;
+  phone: string;
+  username: string;
+  website: string;
+}
