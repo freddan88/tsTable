99e09a55bad4155f3c722ebc007b168bf8451cb8
freddan88 <fredrik@leemann.se>
2023-10-30 00:14:38 +0100

    Add utility-type to solve error

--- src/pages/users/hooks/useUserPageModal.ts
+++ src/pages/users/hooks/useUserPageModal.ts
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { getSingleUser } from "../helpers/userPageRequests";
 import { RouteParams } from "../helpers/userPage.types";
 
-export default function useUserPageModal({ id, action }: RouteParams) {
+export default function useUserPageModal({ id, action }: Partial<RouteParams>) {
   const { data: response, isLoading } = useQuery({
     queryKey: ["user", action, id],
     queryFn: () => getSingleUser(id),
