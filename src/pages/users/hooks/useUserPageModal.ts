06ee110a366feb0afb4852bad7011caf67d44baa
freddan88 <fredrik@leemann.se>
2023-10-30 00:12:40 +0100

    Importing type from shared-file

--- src/pages/users/hooks/useUserPageModal.ts
+++ src/pages/users/hooks/useUserPageModal.ts
@@ -1,17 +1,13 @@
 import { useQuery } from "react-query";
 import { getSingleUser } from "../helpers/userPageRequests";
+import { RouteParams } from "../helpers/userPage.types";
 
-type HookModalTypes = {
-  action?: string;
-  id?: string;
-};
-
-export default function useUserPageModal({ id, action }: HookModalTypes) {
+export default function useUserPageModal({ id, action }: RouteParams) {
   const { data: response, isLoading } = useQuery({
-    queryKey: ["user", id],
+    queryKey: ["user", action, id],
     queryFn: () => getSingleUser(id),
     enabled: action === "edit",
   });
 
-  return { isLoading, data: response?.data || [] };
+  return { isLoading, data: response?.data };
 }
