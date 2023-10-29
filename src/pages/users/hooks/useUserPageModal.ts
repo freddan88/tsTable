5ffca37f0c49eb9de5acd9c3ebbacc4034468cdc
freddan88 <fredrik@leemann.se>
2023-10-29 23:33:12 +0100

    Add types and change when enabled

--- src/pages/users/hooks/useUserPageModal.ts
+++ src/pages/users/hooks/useUserPageModal.ts
@@ -2,14 +2,15 @@ import { useQuery } from "react-query";
 import { getSingleUser } from "../helpers/userPageRequests";
 
 type HookModalTypes = {
+  action?: string;
   id?: string;
 };
 
-export default function useUserPageModal({ id }: HookModalTypes) {
+export default function useUserPageModal({ id, action }: HookModalTypes) {
   const { data: response, isLoading } = useQuery({
     queryKey: ["user", id],
     queryFn: () => getSingleUser(id),
-    enabled: Boolean(id?.toString()),
+    enabled: action === "edit",
   });
 
   return { isLoading, data: response?.data || [] };
