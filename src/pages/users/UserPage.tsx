ba8786166f0a99afc8641a74d8f4cf1a3c0fdeaa
freddan88 <fredrik@leemann.se>
2023-10-30 00:20:17 +0100

    Add example-child to pass in shelf

--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -25,7 +25,24 @@ export default function UserPage() {
   return (
     <>
       <Table table={{ columns, data, loading: isLoading }} />
-      <Shelf3 onClose={handleClose} id={shelfId} />
+      <Shelf3 onClose={handleClose} id={shelfId}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo ipsa
+          nostrum fuga eum cum est? Illum, ex. Corporis odit dolore perferendis
+          doloremque nisi, exercitationem sequi dicta quaerat nostrum
+          voluptatibus rem. Ut, ullam nesciunt. Ab temporibus itaque nam, hic
+          porro non ex corporis adipisci aspernatur quia aut voluptatum fugit
+          dolor distinctio praesentium maiores ad vero consequuntur quo nemo!
+          Reiciendis, iure asperiores! Corporis officia veritatis, voluptatibus
+          aliquam, quasi vel minus, vero reiciendis sint accusantium iusto!
+          Assumenda porro repellendus perferendis rem quod corporis hic deserunt
+          cum, consectetur omnis, odit incidunt accusamus voluptas rerum.
+          Obcaecati eveniet est commodi architecto, consequatur tenetur.
+          Aspernatur dignissimos nemo laborum laboriosam, ullam debitis, porro
+          sunt esse deserunt incidunt cupiditate, adipisci amet facere
+          blanditiis repellat quod at vitae reiciendis perspiciatis?
+        </p>
+      </Shelf3>
       <Outlet />
     </>
   );
