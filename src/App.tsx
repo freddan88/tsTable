6c7c27425b2594055423f5946d2471da47dc6973
freddan88 <fredrik@leemann.se>
2023-10-29 23:31:34 +0100

    Changed element-type to symantic-meaning

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import routes from "./routes/routes";
 
 function App() {
   return (
-    <div className="max-w-4xl mx-auto">
+    <main className="max-w-4xl mx-auto">
       <RouterProvider router={routes} />
-    </div>
+    </main>
   );
 }
 
